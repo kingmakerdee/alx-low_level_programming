032303eacf771d50f6f07778c00c5d733e787247
kingmakerdee <denzelndlovu63@gmail.com>
2022-09-08 19:22:27 -0700

    C programme prints new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints line of text
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
