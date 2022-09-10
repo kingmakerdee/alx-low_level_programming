5b8b951b28311c0c84d8a2082177daec85fa2f5e
kingmakerdee <denzelndlovu63@gmail.com>
2022-09-09 19:34:02 -0700

    Print single numbers in base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:always 0
+ *
+ * print single numbers from 0 in base 10
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10 ; a++)
+		putchar('0' + a);
+	putchar('\n');
+	return (0);
+}
