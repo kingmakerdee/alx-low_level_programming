2c3aa986febc46b827af78690260c39bd47a9d8c
kingmakerdee <denzelndlovu63@gmail.com>
2022-09-14 10:33:47 -0700

    add ints a and b and print sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,13 +6,11 @@
  * @a: int to be added to b
  * @b: int to be added to a
  *
- * Return: (c) sum of a + b
+ * Return: sum of a + b
  *
  */
 int add(int a, int b)
 {
-	int a;
-	int b;
 	int c;
 
 	c = a + b;
